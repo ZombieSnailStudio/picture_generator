cdd4cc5ba410d237b97293bacdfa5821d1e60475
svonton <56838368+svonton@users.noreply.github.com>
2020-02-10 17:24:13 +0200

    test uppload

---
+++ picture_generator/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace picture_generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.createbtn = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.savebtn = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.filename = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.path = new System.Windows.Forms.Button();
+            this.pathdisplay = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // createbtn
+            // 
+            this.createbtn.Location = new System.Drawing.Point(1029, 106);
+            this.createbtn.Name = "createbtn";
+            this.createbtn.Size = new System.Drawing.Size(75, 23);
+            this.createbtn.TabIndex = 0;
+            this.createbtn.Text = "create";
+            this.createbtn.UseVisualStyleBackColor = true;
+            this.createbtn.Click += new System.EventHandler(this.createbtn_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(694, 740);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1030, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(74, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "0";
+            // 
+            // savebtn
+            // 
+            this.savebtn.Location = new System.Drawing.Point(1002, 683);
+            this.savebtn.Name = "savebtn";
+            this.savebtn.Size = new System.Drawing.Size(102, 45);
+            this.savebtn.TabIndex = 3;
+            this.savebtn.Text = "save";
+            this.savebtn.UseVisualStyleBackColor = true;
+            this.savebtn.Click += new System.EventHandler(this.savebtn_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(758, 192);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(238, 193);
+            this.textBox2.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(758, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(141, 136);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // filename
+            // 
+            this.filename.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.filename.Location = new System.Drawing.Point(772, 597);
+            this.filename.Multiline = true;
+            this.filename.Name = "filename";
+            this.filename.Size = new System.Drawing.Size(190, 34);
+            this.filename.TabIndex = 6;
+            // 
+            // path
+            // 
+            this.path.Location = new System.Drawing.Point(1002, 485);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(75, 34);
+            this.path.TabIndex = 7;
+            this.path.Text = "select path";
+            this.path.UseVisualStyleBackColor = true;
+            this.path.Click += new System.EventHandler(this.path_Click);
+            // 
+            // pathdisplay
+            // 
+            this.pathdisplay.Location = new System.Drawing.Point(772, 511);
+            this.pathdisplay.Multiline = true;
+            this.pathdisplay.Name = "pathdisplay";
+            this.pathdisplay.Size = new System.Drawing.Size(190, 34);
+            this.pathdisplay.TabIndex = 8;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            ".png",
+            ".jpeg"});
+            this.comboBox1.Location = new System.Drawing.Point(772, 671);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(938, 12);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(166, 62);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Text = "Введите размер матрицы.\r\nПример: 8 - матрица 8х8.\r\nПоддерживается размер до 32(вк" +
+    "лючая 32)";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(772, 485);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(190, 20);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.Text = "Путь сохранения рисунка";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(772, 571);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(190, 20);
+            this.textBox5.TabIndex = 12;
+            this.textBox5.Text = "Введите название рисунка";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Enabled = false;
+            this.textBox6.Location = new System.Drawing.Point(772, 645);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(190, 20);
+            this.textBox6.TabIndex = 13;
+            this.textBox6.Text = "Выберите формат рисунка";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1116, 740);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pathdisplay);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.filename);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.savebtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.createbtn);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "picture_generator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button createbtn;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button savebtn;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox filename;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button path;
+        private System.Windows.Forms.TextBox pathdisplay;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ picture_generator/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace picture_generator
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        int n;
+        string default_tag = "0";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void createbtn_Click(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.Controls.Clear();
+            n = int.Parse(textBox1.Text.ToString());
+            if(n>0 && n<=32)
+            {
+                flowLayoutPanel1.Width = n * 22;
+                for (i = 1; i < (n * n) + 1; i++)
+                {
+                    flowLayoutPanel1.Controls.Add(btn(i));
+                }
+            }
+            else
+            {
+                return;
+            }
+            
+        }
+        Button btn (int i)
+        {
+            Button b = new Button();
+            b.Name = i.ToString();
+            b.Width = 16;
+            b.Height = 16;
+            //b.Text = i.ToString();
+            b.Tag = 0;
+            b.BackColor = Color.White;
+            b.Click += b_Click;
+            return b;
+        }
+
+        private void b_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.Tag.ToString() == default_tag)
+            {
+                b.BackColor = Color.Gray;
+                b.Tag = 1;
+            }
+            else
+            {
+                b.BackColor = Color.White;
+                b.Tag = 0;
+            }
+        }
+
+        private void savebtn_Click(object sender, EventArgs e)
+        {
+            var list = new List<int>();
+            foreach (Control btn in flowLayoutPanel1.Controls)
+            {
+                if(btn.Tag.ToString() == default_tag)
+                {
+                    list.Add(0);
+                }
+                else
+                {
+                    list.Add(1);
+                }
+            }
+            int[] array = list.ToArray();
+            textBox2.Width = n * 10;
+            textBox2.Height = n * 14;
+            textBox2.Text = string.Join(" ", array);
+            int t = 0, k = 1;
+            Bitmap bmp = new Bitmap(n, n);
+            for (int x = 0; x < n; x++)
+            {
+                for (int y = 0; y < n; y++)
+                {
+                    if (array[t] == 0)
+                    {
+                        bmp.SetPixel(y, x, Color.FromArgb(255, 255, 255));
+                    }
+                    else if (array[t] == 1)
+                    {
+                        bmp.SetPixel(y, x, Color.FromArgb(0, 0, 0));
+                    }
+                    t++;
+                    k++;
+                }
+                k = 0;
+            }
+            pictureBox1.Width = n;
+            pictureBox1.Height = n;
+            pictureBox1.Image = bmp;
+            string fl = filename.Text.ToString();
+            string cb = comboBox1.Text;
+            string form = String.Format("{0}\\{1}{2}", folderBrowserDialog1.SelectedPath,fl,cb);
+            bmp.Save(form);
+        }
+        private void path_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            pathdisplay.Text = folderBrowserDialog1.SelectedPath;
+        }
+    }
+}
